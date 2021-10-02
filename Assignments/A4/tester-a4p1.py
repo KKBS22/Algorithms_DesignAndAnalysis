8a9534e8e6d2cb0687e747fffcccbc0cb9a77942
Kaushal Kiran Bangalore Sampath Kumar <kkbangaloresampathkumar@uwaterloo.ca>
2021-10-02 08:38:58 -0400

    Tester update

--- Assignments/A4/tester-a4p1.py
+++ Assignments/A4/tester-a4p1.py
@@ -11,7 +11,6 @@ If you set this file to executable, and have the a4p1.py file in
 the same folder, then you should be able to run this.
 """
 
-
 def isTree(G, T):
     # Basic tests first against G
     if len(T) != len(G):
@@ -26,8 +25,8 @@ def isTree(G, T):
     colour = list()
     pi = list()
     for i in range(len(T)):
-        colour.append(False)  # False == WHITE
-        pi.append(-1)  # -1 == NIL
+        colour.append(False) # False == WHITE
+        pi.append(-1) # -1 == NIL
     Q = list()
     Q.append(0)
     colour[0] = True
@@ -38,16 +37,15 @@ def isTree(G, T):
                 colour[v] = True
                 pi[v] = u
                 Q.append(v)
-            elif pi[u] != v:  # This is the key check
+            elif pi[u] != v: # This is the key check
                 return False
     return True
-
 
 def isDifferent(P, Q):
     # Are the graphs P and Q different?
     if(len(P) != len(Q)):
         return True
-
+    
     for u in range(len(P)):
         for v in P[u]:
             if v not in Q[u]:
@@ -57,9 +55,8 @@ def isDifferent(P, Q):
         for v in Q[u]:
             if v not in P[u]:
                 return True
-
+    
     return False
-
 
 def graphCopy(G):
     H = list()
@@ -69,11 +66,10 @@ def graphCopy(G):
             (H[i]).append(j)
     return H
 
-
 def main():
-    print('Test 1:', end=' ')
-    G = [[2, 3, 4], [2, 3], [0, 1], [0, 1, 4], [0, 3]]
-    T = [[2, 3], [3], [0], [0, 1, 4], [3]]
+    print('Test 1:', end = ' ')
+    G = [[2,3,4],[2,3],[0,1],[0,1,4],[0,3]]
+    T = [[2,3],[3],[0],[0,1,4],[3]]
     # Make a copies of G and T
     H = graphCopy(G)
     S = graphCopy(T)
@@ -84,10 +80,9 @@ def main():
     else:
         print('Failed')
 
-    print('Test 2:', end=' ')
-    G = [[1, 7], [0, 2, 7], [1, 3, 5, 8], [2, 4], [3, 5],
-         [2, 3, 4, 6], [5, 7, 8], [0, 1, 6, 8], [2, 6, 7]]
-    T = [[1], [0, 2], [1, 3, 5, 8], [2, 4], [3], [2, 6], [5, 7], [6], [2]]
+    print('Test 2:', end = ' ')
+    G = [[1,7],[0,2,7],[1,3,5,8],[2,4,5],[3,5],[2,3,4,6],[5,7,8],[0,1,6,8],[2,6,7]]
+    T = [[1],[0,2],[1,3,5,8],[2,4],[3],[2,6],[5,7],[6],[2]]
     # Make a copies of G and T
     H = graphCopy(G)
     S = graphCopy(T)
@@ -98,7 +93,7 @@ def main():
     else:
         print('Failed')
 
-    print('Test 3:', end=' ')
+    print('Test 3:', end = ' ')
     G = [[2, 3, 4], [2], [0, 1], [0], [0]]
     T = [[2, 3, 4], [2], [0, 1], [0], [0]]
     Tprime = anotherst(G, T)
@@ -107,7 +102,6 @@ def main():
         print('Passed')
     else:
         print('Failed')
-
 
 if __name__ == '__main__':
     main()
